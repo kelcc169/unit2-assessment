390ea184b4f477d0580200c0d952b80f5aa7968d
Kelsey Cox <kelseycox@Lucy.local>
2019-07-09 14:24:02 -0700

    delevired

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const db = require('./models')
 
 const app = express();
 
@@ -10,7 +11,33 @@ app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
 
+//GET / - show page!
+app.get('/', function (req, res) {
+    db.widget.findAll().then(function (widgets) {
+        res.render('index', {widgets})
+    });
+});
 
+//POST /add - add a widget
+app.post('/add', function (req, res) {
+    db.widget.create({
+        description: req.body.description,
+        quantity: req.body.quantity
+    }).then(function (data) {
+        res.redirect('/')
+    });
+});
+
+//DELETE /delete - delete a widget
+app.delete('/delete/:id', function (req, res) {
+    db.widget.destroy({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    }).then(function (data) {
+        res.redirect('/')
+    });
+});
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
 
